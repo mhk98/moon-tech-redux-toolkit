35653f8ebc07afc977ff179626ac1d540d434713
mhk98 <mohsinkabirseo@gmail.com>
2023-04-15 18:20:13 +0600

    toolkit moontech

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+// import store from "./redux/store";
+import routes from "./routes/routes";
+import store from "./app/store";
+
+function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Main/Home.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductCard from "../../components/ProductCard";
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  // const dispatch = useDispatch();
+  // const filters = useSelector((state) => state.filter.filters);
+  // const { brands, stock } = filters;
+  // console.log("filters", brands);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.data));
+  }, []);
+
+  return (
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
+        <button className={`border px-3 py-2 rounded-full font-semibold `}>
+          In Stock
+        </button>
+        <button
+          className={`border px-3 py-2 rounded-full font-semibold 
+
+          `}
+        >
+          AMD
+        </button>
+        <button className={`border px-3 py-2 rounded-full font-semibold `}>
+          Intel
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
+        {products.map((product) => (
+          <ProductCard product={product}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
